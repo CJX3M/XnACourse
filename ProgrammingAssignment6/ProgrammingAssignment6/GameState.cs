caf2c387f5165217b94ddab94c8ad0c5c5a33e03
Carlos <cjlc@hotmail.com>
2021-10-15 16:51:03 -0700

    Assigment 6

---
+++ ProgrammingAssignment6/ProgrammingAssignment6/GameState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgrammingAssignment6
+{
+    /// <summary>
+    /// An enumeration of the possible game states
+    /// </summary>
+    public enum GameState
+    {
+        CheckingHandOver,
+        DealerHitting,
+        DisplayingHandResults,
+        Exiting,
+        PlayerHitting,
+        WaitingForDealer,
+        WaitingForPlayer
+    }
+}
